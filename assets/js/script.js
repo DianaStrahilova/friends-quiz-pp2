b9a44dfbb7083279f10984f4d220e0e5d4b2cb3b
DianaStrahilova <159344709+DianaStrahilova@users.noreply.github.com>
2024-05-12 23:54:51 +0000

    Add a question array

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,11 @@ const modal = document.getElementById("modal");
 const openModal = document.getElementById("openModal");
 const closeModal = document.querySelector('.closeModal');
 
+const startButton = document.getElementById("startButton");
+const playAgain = document.getElementById("playAgain");
+const gameEnd = document.getElementById('gameEnd');
+const homeElement = document.querySelector('.home');
+
 openModal.addEventListener('click', () => {
     modal.showModal();
 })
@@ -11,7 +16,17 @@ closeModal.addEventListener('click', () => {
     modal.close();
 })
 
-// Questions Array
+playAgain.addEventListener('click', () => {
+    gameEnd.classList.add('hidden');
+    homeElement.classList.remove('hidden');
+    document.getElementById('userName').value = '';
+});
+
+startButton.addEventListener('click', () => {
+    startQuiz();
+})
+
+
 const questions = [
     {
         question: "What is the name of Ross’s second wife?",
@@ -65,17 +80,12 @@ const questions = [
     }
 ]
 
-// DOM Elements 
-
-const startButton = document.getElementById("startButton");
-const playAgain = document.getElementById("playAgain");
-const gameEnd = document.getElementById('gameEnd');
-const homeElement = document.querySelector('.home');
 const questionElement = document.getElementById('question');
 const answersElement = document.getElementById('answers');
 const scoreValue = document.getElementById('value');
 const incorrectAnswersElement = document.querySelector('#incorrect span');
 const gameArea = document.getElementById('gameArea');
+
 const optionsElement = [
     document.getElementById('answer0'),
     document.getElementById('answer1'),
@@ -87,4 +97,77 @@ let userName = '';
 let currentQuestionIndex = 0;
 let score = 0;
 let incorrectAnswers = 0;
+
+function startQuiz() {
+    userName = document.getElementById('userName').value;
+    currentQuestionIndex = 0;
+    score = 0;
+    incorrectAnswers = 0;
+
+    scoreValue.innerHTML = 0;
+    incorrectAnswersElement.innerHTML = 0;
+
+    homeElement.classList.add('hidden');
+
+    gameArea.classList.remove('hidden');
+
+
+    loadQuestion();
+}
+
+function loadQuestion() {
+    if (currentQuestionIndex >= questions.length) {
+        gameArea.classList.add('hidden');
+        gameEnd.classList.remove('hidden');
+        document.getElementById('gameEndText').innerHTML = `Congratulations <span>${userName}</span>! You completed the quiz!`;
+        return;
+    }
+
+    answersElement.innerHTML = '';
+
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + '.' + currentQuestion.question;
+
+    currentQuestion.options.forEach(answer => {
+        const button = document.createElement('button');
+        button.innerHTML = answer;
+        button.classList.add('optionBtn');
+        button.addEventListener('click', () => {
+            checkAnswer(button);
+        });
+        answersElement.appendChild(button);
+    });
+}
+
+function checkAnswer(button) {
+    const answer = button.innerHTML;
+    const answerIndex = questions[currentQuestionIndex].options.indexOf(answer);
+
+    // Lock buttons
+    document.querySelectorAll('.optionBtn').forEach(button => {
+        button.disabled = true;
+    });
+
+    if (answerIndex === questions[currentQuestionIndex].correctAnswer) {
+        score++;
+        scoreValue.innerHTML = score;
+        button.classList.add('correctAnswer');
+    } else {
+        button.classList.add('incorrectAnswer');
+        incorrectAnswers++;
+        incorrectAnswersElement.innerHTML = incorrectAnswers;
+
+        document.querySelectorAll('.optionBtn').forEach(button => {
+            if (button.textContent === questions[currentQuestionIndex].options[questions[currentQuestionIndex].correctAnswer]) {
+                button.classList.add('correctAnswer');
+            }
+        });
+    }
+
+    setTimeout(() => {
+        currentQuestionIndex++;
+        loadQuestion();
+    }, 2000);
+}
 
