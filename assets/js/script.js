d2264f70a3879a8d13d015130ce531f2286cd79e
DianaStrahilova <159344709+DianaStrahilova@users.noreply.github.com>
2024-05-12 23:26:17 +0000

    Add a favicon and call DOM elements in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Modal
 const modal = document.getElementById("modal");
 const openModal = document.getElementById("openModal");
 const closeModal = document.querySelector('.closeModal');
@@ -9,3 +10,81 @@ openModal.addEventListener('click', () => {
 closeModal.addEventListener('click', () => {
     modal.close();
 })
+
+// Questions Array
+const questions = [
+    {
+        question: "What is the name of Ross’s second wife?",
+        options: ["Carol", "Emily", "Rachel", "Susan"],
+        correctAnswer: 1
+    },
+    {
+        question: "Which character famously said, “We were on a break”?",
+        options: ["Joey", "Chanlder", "Ross", "Mike"],
+        correctAnswer: 2
+    },
+    {
+        question: "What is the title of the theme song of F.R.I.E.N.D.S?",
+        options: ["I'll be there for you", "Smelly Cat", "Central Perk", "The One With All The Friends"],
+        correctAnswer: 0
+    },
+    {
+        question: "What does Joey never share?",
+        options: ["His books", "His food", "His DVDs", "His clothes"],
+        correctAnswer: 1
+    },
+    {
+        question: "What is Chandler’s middle name?",
+        options: ["Matthew", "Marvin", "Michael", "Muriel"],
+        correctAnswer: 3
+    },
+    {
+        question: "Which character has a twin?",
+        options: ["Rachel", "Ross", "Phoebe", "Joey"],
+        correctAnswer: 2
+    },
+    {
+        question: "What job does Ross have?",
+        options: ["Chef", "Actor", "Copywriter", "Paleontologist"],
+        correctAnswer: 3
+    },
+    {
+        question: "What is the name of Joey’s stuffed penguin?",
+        options: ["Waddle", "Pingu", "Huggsy", "Mr.Flipper"],
+        correctAnswer: 2
+    },
+    {
+        question: "What game do Joey and Chandler play that causes them to win Monica and Rachel’s apartment?",
+        options: ["Poker", "Football", "Cups", "Quiz Game"],
+        correctAnswer: 3
+    },
+    {
+        question: "In which city is “F.R.I.E.N.D.S” primarily set?",
+        options: ["Los Angeles", "Miami", "New York City", "Chicago"],
+        correctAnswer: 2
+    }
+]
+
+// DOM Elements 
+
+const startButton = document.getElementById("startButton");
+const playAgain = document.getElementById("playAgain");
+const gameEnd = document.getElementById('gameEnd');
+const homeElement = document.querySelector('.home');
+const questionElement = document.getElementById('question');
+const answersElement = document.getElementById('answers');
+const scoreValue = document.getElementById('value');
+const incorrectAnswersElement = document.querySelector('#incorrect span');
+const gameArea = document.getElementById('gameArea');
+const optionsElement = [
+    document.getElementById('answer0'),
+    document.getElementById('answer1'),
+    document.getElementById('answer3'),
+    document.getElementById('answer3')
+];
+
+let userName = '';
+let currentQuestionIndex = 0;
+let score = 0;
+let incorrectAnswers = 0;
+
